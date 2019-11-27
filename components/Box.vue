8ed016da774e334c32bb178e2a6b71e12819eccf
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-11-27 12:43:27 +0700

    finish!

---
+++ components/Box.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    <div class="d-flex align-center justify-center display-1 text-center font-weight-light" style="height: 100%;">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  background-color: rgba($color: #fff, $alpha: .75);
+  height: 80%;
+  width: 80%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,63 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
+  <v-container fluid class="pa-0"
+    style="height: 100%;"
   >
-    <v-flex
-      xs12
-      sm8
-      md6
+    <v-row
+      align="center"
+      justify="center"
+      style="height: 100%;"
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
+      <box>
+        {{ prayers[selected] }}
+      </box>
+    </v-row>
+    <div class="d-flex justify-space-between mt-n10">
+      <div>
+        <v-btn
+          v-show="selected !== 0"
+          text
+          @click="selected = selected - 1"
+        >
+          Prev
+        </v-btn>
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      <div>
+        <v-btn
+          v-show="selected !== (prayers.length - 1)"
+          text
+          @click="selected = selected + 1"
+        >
+          Next
+        </v-btn>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import Box from '~/components/Box'
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Box
+  },
+  data () {
+    return {
+      prayers: [
+        'Untuk Noor',
+        'ku panjatkan doa,',
+        'agar senantiasa diberi ruangan untuk berkembang dan belajar,',
+        'belajar dari naik turun nya hidup,',
+        'hidup memang unexpected, tapi kamu bisa menikmati setiap detiknya,',
+        'hidup terakselerasi year-by-year,',
+        'terus explore hal baru di luar zona nyaman,',
+        'paham mana buruk mana benar, mengambil keputusan dengan bijak.',
+        'Keep smile, you deserved to be happy :)',
+        'Somebody loves you',
+        'Keep inspires other',
+        'from your friend Babang'
+      ],
+      selected: 0
+    }
   }
 }
 </script>
